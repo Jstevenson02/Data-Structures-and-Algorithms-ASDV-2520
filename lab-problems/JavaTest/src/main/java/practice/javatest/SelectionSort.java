0e6a8460941034f8a030e825b701c4906efdd5bb
Jacob <jacobryanstevenson@gmail.com>
2022-09-18 13:12:51 -0500

    pushing

--- lab-problems/JavaTest/src/main/java/practice/javatest/SelectionSort.java
+++ lab-problems/JavaTest/src/main/java/practice/javatest/SelectionSort.java
@@ -20,37 +20,27 @@ class SelectionSort {
         return (a[i] < a[k]) ? i : k;
     }
 
-    // Recursive selection sort. n is size of a[] and index
-    // is index of starting element.
     static void selectionSortR(int a[], int n, int index) {
 
-        // Return when starting and size are same
         if (index == n) {
             return;
         }
 
-        // calling minimum index function for minimum index
         int k = minIndex(a, index, n - 1);
 
-        // Swapping when index nd minimum index are not same
         if (k != index) {
-            // swap
             int temp = a[k];
             a[k] = a[index];
             a[index] = temp;
         }
-        // Recursively calling selection sort function
         selectionSortR(a, n, index + 1);
     }
 
-    // Driver method
     public static void main(String args[]) {
         int arr[] = {8, 2, 1, 1, 7, 4, -1, 50, 49};
 
-        // Calling function
         selectionSortR(arr, arr.length, 0);
 
-        //printing sorted array
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
